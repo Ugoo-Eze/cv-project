04219e16a9c87db9ad0419cb65ec39d06b1bf805
Ezeugo <e.ugotex@gmail.com>
2022-04-17 22:42:38 -0700

    added new components

---
+++ src/components/Education.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Education.css'
+
+function Education() {
+  return (
+    <section>Education</section>
+  )
+}
+
+export default Education
---
+++ src/components/Experience.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Experience.css'
+
+function Experience() {
+  return (
+    <section>Experience</section>
+  )
+}
+
+export default Experience
